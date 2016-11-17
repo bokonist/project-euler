b4314b048cd9d62a38ce168f000190122e5182da
bokonist <jayrm1995@gmail.com>
2016-11-18 01:42:29 +0530

    solved problem 2

---
+++ problem2.cpp
@@ -0,0 +1,37 @@
+//https://projecteuler.net/problem=2
+#include "bits/stdc++.h"
+using namespace std;
+/* useful utility macros */
+#define MIN(a,b,res) ((a)>(b))? (res)=(b):(res)=(a)
+#define MAX(a,b,res) ((a)<(b))? (res)=(b):(res)=(a)
+#define printArray(a,l,n) for(int(i)=(l);(i)<int(n);i++) { cout<<(a[i])<<' ';}
+#define printWholeArray(a) for(int(i)=0;(i)<int((sizeof(a)/sizeof(a[0])));i++) { cout<<(a[i])<<' ';}
+#define repeat(n) for(int (i)=0;(i)<(int)(n);++(i)) //repeat n times
+#define repeatBounds(l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i)) //repeat lower to upperbound
+#define until(x,val) for(int(i) = int(x); int(x)!=int(val); (i<val)?x++:x--) //until x becomes val through increment or decrement
+
+/*some constants*/
+static const long double PI = 3.142857143;
+
+/*some shorthands*/
+typedef long long ll;
+#define op cout<< // output to stdout
+#define ip cin>> //input from stdin
+#define err cerr<< //output to stderr
+#define nl cout<<"\n"; //newline
+
+int main()
+{
+	long long val[3]={1,2,3};
+	long long sum=2;
+	while(val[2]< 4000000)
+	{
+		val[0]=val[1];
+		val[1]=val[2];
+		val[2]=val[0]+val[1];
+		if(!(val[2]&1))
+			sum+=val[2];
+	}
+	op sum;
+	return 0;
+}
